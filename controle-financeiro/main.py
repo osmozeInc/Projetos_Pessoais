48b873dde72a4d1ea4111384b7eee6b2bf70aa43
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2024-09-01 16:02:42 -0300

    estruturando app

---
+++ controle-financeiro/main.py
@@ -0,0 +1,10 @@
+import sqlite3
+from kivy.app import App
+from kivy.lang import Builder
+
+cliente = {'nome': '', 'cpf': '', 'ddd': '', 'numero': '', 'email': '', 'data': '', 'CEP': '', 'rua': '', 'Ncasa': '','senha': '','saldo': 0}
+
+
+class Sistema_Bancario(App):
+
+
