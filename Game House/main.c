b997537b745b42d3a048a78b2723a68935389698
Caio Monte <caiomm076@gmail.com>
2024-12-15 16:27:45 -0300

    update

--- Game House/main.c
+++ Game House/main.c
@@ -4,44 +4,60 @@
 
 #define VERDE "\033[32m"
 
-void carregando(int num_porcent){
-    char bar_porcent[33];
-
-    if (num_porcent == 0)
-        strcpy(bar_porcent, "                                 |   ");
-    else if (num_porcent == 20)
-        strcpy(bar_porcent, "#######                          |  ");
-    else if (num_porcent == 40)
-        strcpy(bar_porcent, "#############                    |  ");
-    else if (num_porcent == 60)
-        strcpy(bar_porcent, "####################             |  ");
-    else if (num_porcent == 80)
-        strcpy(bar_porcent, "###########################      |  ");
-    else if (num_porcent == 100)
-        strcpy(bar_porcent, "#################################| ");
+void carregando(int num_porcent_bottom, int num_porcent_top){
+    int start = 0;
 
     system("cls");
-    printf(VERDE"Iniciando Sistema...\n"
-           "|=================================|======|\n"
-           "|%s%d%% |\n"
-           "|=================================|======|", bar_porcent, num_porcent);
+    printf(VERDE"Iniciando Sistema...\n\n\n"
+           "|=================================|======|\n");
+
+    if (num_porcent_bottom > 3)
+        start = num_porcent_bottom / 3;
+
+    for (int i = 1; i <= (num_porcent_top - num_porcent_bottom)/3; i++)      // cria os espaços
+    {
+        printf("|");
+        for (int j = 0; j < i + start; j++)    //cria a barra de porcentagem
+        printf("#");
+        for (int j = i + start; j < 33; j++)      // cria os espaços
+            printf(" ");
+
+        if (((i + start) * 3) < 10)       // cria o espaço antes do numero de porcentagem e o numero
+            printf("|%4d%% |", num_porcent_bottom + (i * 3));
+        else if ((i + start) * 3 >= 10 && (i + start) * 3 < 99)
+            printf("|%4d%% |", num_porcent_bottom + (i * 3));
+        else 
+            printf("|%4d%% |", num_porcent_bottom + (i * 3) + 1);
+
+        printf("\n|=================================|======|\n");
+        Sleep(200);
+
+
+        if (i < (num_porcent_top - num_porcent_bottom)/3)
+        {
+            printf("\e[A\e[K");
+            printf("\e[A\e[K");
+        }
+            
+    }
 }
+
 
 int main(){
     system("chcp 65001");
 
-    carregando(0);
     // compila todos os arquivos com as dependencias e executa o programa
     system("gcc configuracoes/configuracoes.c ranking/ranking.c game_house.c -o game_house.exe");
-    carregando(20);
+    carregando(0, 20);
     system("gcc configuracoes/configuracoes.c ranking/ranking.c Jogos/jogo_da_velha.c -o Jogos/jogo_da_velha.exe");
-    carregando(40);
+    carregando(20, 40);
     system("gcc configuracoes/configuracoes.c ranking/ranking.c Jogos/jogo_de_dados.c -o Jogos/jogo_de_dados.exe");
-    carregando(60);
+    carregando(40, 60);
     system("gcc configuracoes/configuracoes.c ranking/ranking.c configuracoes/configurar_cores.c -o configuracoes/configurar_cores.exe");
-    carregando(80);
+    carregando(60, 80);
     system("gcc configuracoes/configuracoes.c ranking/ranking.c ranking/placar_ranking.c -o ranking/placar_ranking.exe");
-    carregando(100);
+    carregando(80, 100);
+
 
     system("game_house.exe");
 }
--- teste.c
+++ teste.c
@@ -6,31 +6,33 @@
 #define VERDE "\033[32m"
 
 void carregando(int num_porcent_bottom, int num_porcent_top){
-    system("cls");
+    int start = 0;
 
+    system("cls");
     printf(VERDE"Iniciando Sistema...\n\n\n"
            "|=================================|======|\n");
+
+    if (num_porcent_bottom > 3)
+        start = num_porcent_bottom / 3;
 
     for (int i = 1; i <= (num_porcent_top - num_porcent_bottom)/3; i++)      // cria os espaços
     {
         printf("|");
-        
-        for (int j = 0; j < i; j++)    //cria a barra de porcentagem
+        for (int j = 0; j < i + start; j++)    //cria a barra de porcentagem
         printf("#");
-
-        for (int j = i; j < 33; j++)      // cria os espaços
+        for (int j = i + start; j < 33; j++)      // cria os espaços
             printf(" ");
 
-        if (num_porcent_top >= 0 && num_porcent_top < 10)       // cria o espaço antes do numero de porcentagem e o numero
-            printf("|    %d%% |", num_porcent_bottom + ((i) * 3));
-        else if (num_porcent_top > 9 && num_porcent_top < 100)
-            printf("|  %d%% |", num_porcent_bottom + ((i) * 3));
+        if (((i + start) * 3) < 10)       // cria o espaço antes do numero de porcentagem e o numero
+            printf("|%4d%% |", num_porcent_bottom + (i * 3));
+        else if ((i + start) * 3 >= 10 && (i + start) * 3 < 99)
+            printf("|%4d%% |", num_porcent_bottom + (i * 3));
         else 
-            printf("| %d%% |", num_porcent_bottom + (i));
+            printf("|%4d%% |", num_porcent_bottom + (i * 3) + 1);
 
         printf("\n|=================================|======|\n");
+        Sleep(200);
 
-        Sleep(500);
 
         if (i < (num_porcent_top - num_porcent_bottom)/3)
         {
@@ -43,7 +45,7 @@ void carregando(int num_porcent_bottom, int num_porcent_top){
 
 int main(){
     //carregando(0, 0);
-    carregando(0, 12);
+    carregando(12, 100);
     // carregando(20, 40);
     // carregando(40, 60);
     // carregando(60, 80);
