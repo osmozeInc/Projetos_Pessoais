dab9e5e652441a59df5399e2677f237f9216fc72
Caio Monte <caiomm076@gmail.com>
2024-12-20 01:16:09 -0300

    game house

--- Game House/main.c
+++ Game House/main.c
@@ -3,17 +3,15 @@
 #include <Windows.h>
 
 #define VERDE "\033[32m"
+#define LIMPAR_TELA "\033[2J\033[H"
 
 
 void carregando(int num_porcent_from, int num_porcent_to, char mensagem[31]){
-    int start = 0;
 
-    system("cls");
-    printf(VERDE"Iniciando Sistema...\n\n\n"
+    printf(LIMPAR_TELA VERDE"Iniciando Sistema...\n\n\n"
            "|=================================|======|\n");
 
-    if (num_porcent_from > 3)
-        start = num_porcent_from / 3;
+        int start = num_porcent_from / 3;
 
     for (int i = 1; i <= (num_porcent_to - num_porcent_from)/3; i++)    // cria os espaços
     {
@@ -24,7 +22,7 @@ void carregando(int num_porcent_from, int num_porcent_to, char mensagem[31]){
         for (int j = i + start; j < 33; j++)    // cria os espaços
             printf(" ");
 
-        if ((i + start) * 3 < 99)       // cria o espaço antes do numero de porcentagem e o numero
+        if ((i + start) * 3 < 99)       // exibe a porcentagem de 0 a 100
             printf("|%4d%% |", num_porcent_from + (i * 3));
         else 
             printf("|%4d%% |", num_porcent_from + (i * 3) + 1);
@@ -37,9 +35,7 @@ void carregando(int num_porcent_from, int num_porcent_to, char mensagem[31]){
 
 
         if (i < (num_porcent_to - num_porcent_from)/3)
-        {
             printf("\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K");
-        }
     }
 }
 
@@ -48,12 +44,12 @@ int main(){
 
     // compila todos os arquivos com as dependencias e executa o programa
     system("gcc configuracoes/configuracoes.c ranking/ranking.c game_house.c -o game_house.exe");
-    carregando(0, 20, "Compilando sistema principal"); // mensagem deve ter até 31 caracteres
+    carregando(0, 21, "Compilando sistema principal"); // mensagem deve ter até 31 caracteres
     system("gcc configuracoes/configuracoes.c ranking/ranking.c Jogos/jogo_da_velha.c -o Jogos/jogo_da_velha.exe");
     system("gcc configuracoes/configuracoes.c ranking/ranking.c Jogos/jogo_de_dados.c -o Jogos/jogo_de_dados.exe");
-    carregando(20, 60, "Carregando jogos");
+    carregando(21, 60, "Carregando jogos");
     system("gcc configuracoes/configuracoes.c ranking/ranking.c configuracoes/configurar_cores.c -o configuracoes/configurar_cores.exe");
-    carregando(60, 80, "Iniciando sistema de cores");
+    carregando(60, 81, "Iniciando sistema de cores");
     system("gcc configuracoes/configuracoes.c ranking/ranking.c ranking/placar_ranking.c -o ranking/placar_ranking.exe");
     carregando(81, 100, "Inserindo sistema de ranquing"); // é ideal que o carregamento que não comece de 0 seja em mutiplos de 3
 
