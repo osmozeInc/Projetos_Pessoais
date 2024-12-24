d4bd0a07122107a3f0299caec4a1082c600cdad5
Caio Monte <caiomm076@gmail.com>
2024-12-24 15:16:58 -0300

    game house

--- terminal/load_bar.c
+++ terminal/load_bar.c
@@ -3,50 +3,41 @@
 #include <Windows.h>
 
 #define VERDE "\033[32m"
+#define LIMPAR_TELA "\033[2J\033[H"
 
-void carregando(int num_porcent_bottom, int num_porcent_top){
-    int start = 0;
+void carregando(int num_porcent_from, int num_porcent_to, char mensagem[31]){
 
-    system("cls");
-    printf(VERDE"Iniciando Sistema...\n\n\n"
+    printf(LIMPAR_TELA VERDE"Iniciando Sistema...\n\n\n"
            "|=================================|======|\n");
 
-    if (num_porcent_bottom > 3)
-        start = num_porcent_bottom / 3;
+        int start = num_porcent_from / 3;
 
-    for (int i = 1; i <= (num_porcent_top - num_porcent_bottom)/3; i++)      // cria os espaços
+    for (int i = 1; i <= (num_porcent_to - num_porcent_from)/3; i++)    // cria os espaços
     {
         printf("|");
-        for (int j = 0; j < i + start; j++)    //cria a barra de porcentagem
-        printf("#");
-        for (int j = i + start; j < 33; j++)      // cria os espaços
+
+        for (int j = 0; j < i + start; j++)     //cria a barra de porcentagem
+            printf("#");
+        for (int j = i + start; j < 33; j++)    // cria os espaços
             printf(" ");
 
-        if (((i + start) * 3) < 10)       // cria o espaço antes do numero de porcentagem e o numero
-            printf("|%4d%% |", num_porcent_bottom + (i * 3));
-        else if ((i + start) * 3 >= 10 && (i + start) * 3 < 99)
-            printf("|%4d%% |", num_porcent_bottom + (i * 3));
+        if ((i + start) * 3 < 99)       // exibe a linha de porcentagem de 0 a 100
+            printf("|%4d%% |", num_porcent_from + (i * 3));
         else 
-            printf("|%4d%% |", num_porcent_bottom + (i * 3) + 1);
+            printf("|%4d%% |", num_porcent_from + (i * 3) + 1);
 
-        printf("\n|=================================|======|\n");
-        Sleep(200);
+        printf("\n|=================================|======|"
+               "\n| %-31s |"
+               "\n|=================================|\n", mensagem);
+
+        Sleep(80);
 
 
-        if (i < (num_porcent_top - num_porcent_bottom)/3)
-        {
-            printf("\e[A\e[K");
-            printf("\e[A\e[K");
-        }
-            
+        if (i < (num_porcent_to - num_porcent_from)/3)
+            printf("\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K");
     }
 }
 
 int main(){
-    //carregando(0, 0);
-    carregando(12, 100);
-    // carregando(20, 40);
-    // carregando(40, 60);
-    // carregando(60, 80);
-    // carregando(80, 100);
+    carregando(0, 21, "Compilando sistema principal"); // mensagem deve ter até 31 caracteres
 }
