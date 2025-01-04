594fec9053c11d3925713877ccd785a3267b90c2
Caio Monte <caiomm076@gmail.com>
2025-01-04 00:36:13 -0300

    gerador de pdf

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-const datePDF = document.querySelector(".datePDF");
-
-const formattedDate = new Intl.DateTimeFormat('pt-BR', {
-    day: '2-digit',
-    month: 'long',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-}).format(new  Date());;
-
-datePDF.innerHTML += formattedDate.replace('às', '-').replace('Às', '-');
 
 
+
+function atualizar_data(){
+    const formattedDate1 = new Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    }).format(new  Date());
+    
+    const datePDF = document.querySelector(".datePDF");
+    datePDF.innerHTML += formattedDate1.replace('às', '-').replace('Às', '-');
+
+    const formattedDate2 = new Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    }).format(new  Date());
+
+    const dateHeader = document.getElementById('data');
+    dateHeader.innerHTML = formattedDate2;
+}
+
+
+
+const btnsave = document.querySelector("#save");
+btnsave.addEventListener("click", function(){
+    let ids = ['pedido', 'validade', 'vendedor', 'codigo-nome', 'cpf-cnpj', 'rg-ie', 'endereco', 'complemento', 'bairro', 'cidade-uf', 'cep', 'telefone', 'contato',  'celular','email'];
+
+    for(let i = 0; i < ids.length; i++){
+        let imput = document.getElementById(ids[i] + '-imput');
+        let output = document.getElementById(ids[i]);
+
+        if(imput){
+            output.textContent = imput.value;
+            console.log('execução: ' + `${i + 1}`);
+        }
+    }
+});
 
 
 
 // download pdf
 const btnpdf = document.querySelector("#download");
-
 btnpdf.addEventListener("click", function(){
     
     let opt = {
@@ -36,3 +63,4 @@ btnpdf.addEventListener("click", function(){
     html2pdf().set(opt).from(element).save();
 });
 
+atualizar_data();
